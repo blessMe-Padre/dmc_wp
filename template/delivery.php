15b13183d899569280e5841627bafa0376054845
Сергей Субботин <zarodiny@yandex.ru>
2023-12-11 16:54:23 +1000

    feat: добавляет доставка

---
+++ template/delivery.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: Доставка и оплата - шаблон
+*/
+?>
+<?php get_header('custom'); ?>
+
+<main class="inner-page-main mt-40 delivery">
+    <div class="container">
+        <div class="flex mb-5">
+            <h1 class="text-xl lg:text-6xl text-jost font-extrabold line uppercase relative">
+                Доставка и оплата</h1>
+        </div>
+        <div class="delivery__wrapper">
+            <div class="delivery__item delivery__item--1">
+                <h2 class="delivery__title">доставка</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+            </div>
+            <div class="delivery__item delivery__item--2">
+                <h2 class="delivery__title delivery__title--green">доставка</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ipsam corrupti aliquid
+                    amet. Iste voluptate, quis officia consectetur ipsum saepe enim ut earum qui praesentium ullam
+                    ducimus delectus odit!</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
